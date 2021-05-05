04750c13e9b5cbc192190dae6beaad469d9f5f72
naimenes33 <71629003+naimenes33@users.noreply.github.com>
2021-05-06 01:11:36 +0300

    sınav sonucu kontrol ediliyor.

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Answer;
 use App\Models\Quiz;
+use App\Models\Result;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MainController extends Controller
 {
@@ -26,7 +29,38 @@ class MainController extends Controller
     }
 
     public function result(Request $request, $slug){
-      return $request->post();
+       $quiz = Quiz::whereSlug($slug)->with('questions')->first() ?? abort(404,'Quiz Bulunamadı');
+      
+
+      $correct=0;
+      foreach($quiz->questions as $question){
+      Answer::create([
+        'user_id'=>auth()->user()->id,
+        'question_id'=>$question->id,
+        'answer'=>$request->post($question->id)
+
+      ]);
+
+      if($question->correct_answer === $request->post($question->id)){
+        $correct += 1 ;   
+      }
+
+      }
+        $point = round(( 100 / count($quiz->questions) ) * $correct);
+        $wrong = count($quiz->questions) - $correct;
+
+
+      Result::create([
+        'user_id'=>auth()->user()->id,
+        'quiz_id'=>$quiz->id,
+        'point'=>$point,
+        'correct'=>$correct,
+        'wrong'=>$wrong,
+
+      ]);
+    
+      return redirect()->route('quiz.detail',$quiz->slug)->withSuccess("başarıyla quiz'i bitirdin.puanın : ".$point);
+
 
     }
 
